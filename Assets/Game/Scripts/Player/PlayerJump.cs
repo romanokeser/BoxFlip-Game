61d90ff7aa620b9dd4002671964fc3526f45dfdb
RomanoK <romanokeser@gmail.com>
2023-04-28 23:56:18 +0200

    Reset level, updated player jump

--- Assets/Game/Scripts/Player/PlayerJump.cs
+++ Assets/Game/Scripts/Player/PlayerJump.cs
@@ -5,10 +5,13 @@ public class PlayerJump : MonoBehaviour
     [SerializeField] private float _jumpTimeThreshold = 0.5f;
     [SerializeField] private float _maxJumpForce = 20f;
     [SerializeField] private AnimationCurve _jumpCurve = AnimationCurve.Linear(0f, 0f, 1f, 1f);
+    [SerializeField] private float _flipSpeed = 5f;
 
     private Rigidbody2D rb;
     private bool isJumping;
     private float jumpStartTime;
+    private bool hasLanded;
+    private bool isInAir;
 
     private void Awake()
     {
@@ -18,14 +21,21 @@ public class PlayerJump : MonoBehaviour
     private void Update()
     {
         CheckInput();
+        if (isInAir)
+        {
+            //Flip();
+        }
     }
 
     private void CheckInput()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping && hasLanded)
         {
             isJumping = true;
             jumpStartTime = Time.time;
+            hasLanded = false;
+            rb.freezeRotation = true;
+            isInAir = true;
         }
 
         if (Input.GetKeyUp(KeyCode.Space) && isJumping)
@@ -34,6 +44,7 @@ public class PlayerJump : MonoBehaviour
             float jumpTime = Time.time - jumpStartTime;
             float jumpForce = CalculateJumpForce(jumpTime);
             Jump(jumpForce);
+            rb.freezeRotation = false;
         }
     }
 
@@ -55,6 +66,25 @@ public class PlayerJump : MonoBehaviour
         {
             xForce = jumpForce;
         }
-        rb.velocity = new Vector2(xForce, jumpForce);
+        rb.velocity = new Vector2(xForce, jumpForce * 2);
+    }
+
+    private void Flip()
+    {
+        transform.Rotate(Vector3.forward, _flipSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            hasLanded = true;
+            // check the angle of the player and rotate it accordingly
+            float angle = transform.rotation.eulerAngles.z;
+            if (angle > 90f && angle < 270f)
+            {
+                transform.Rotate(0, 0, 180f);
+            }
+        }
     }
 }
